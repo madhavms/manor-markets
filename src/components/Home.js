11a200d5a0c40a2d36e5b08eb99e4c60c95afd33
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-30 14:57:39 +0000

    Home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Home page!</h1>
+      <p>This is the home component.</p>
+    </div>
+  );
+}
+
+export default Home;
