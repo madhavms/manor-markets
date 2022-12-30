4ca8486b423e678ad37b31d7cda17df4b0416b7f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-30 14:57:26 +0000

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import ManorMarketsNavbar from './components/NavBar';
+import "./App.css";
+import ManorMarketsNavbar from "./components/NavBar";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import {Products,Contact} from "./components/ProductsContact";
+
 
 function App() {
   return (
-    <div className="App">
-    <ManorMarketsNavbar/>
-    </div>
+    <BrowserRouter>
+      <ManorMarketsNavbar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/products" element={<Products/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
