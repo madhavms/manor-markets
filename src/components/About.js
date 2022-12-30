1e949fa3dfd1a6acaf0c76f5e051500eb262e869
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-30 14:57:34 +0000

    About component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>This is the about component.</p>
+    </div>
+  );
+}
+
+export default About;
