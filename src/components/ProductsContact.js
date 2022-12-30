e16ec86782b1274238be1ec892b1eeee034f0d7e
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-30 14:57:56 +0000

    ProductsContact component

---
+++ src/components/ProductsContact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export function Products() {
+  return (
+    <div className="products-container">
+      <h1>Products</h1>
+      <p>This is the products component.</p>
+    </div>
+  );
+}
+
+export function Contact() {
+  return (
+    <div className="contact-container">
+      <h1>Contact</h1>
+      <p>This is the contact component.</p>
+    </div>
+  );
+}
+
