90dac2aa25ddfe34619381619b57926962acf294
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-01 09:57:27 +0000

    Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {
-  Container,
-  Typography,
-  TextField,
-  Button
-} from '@material-ui/core';
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Typography, TextField, Button } from "@material-ui/core";
+import useAuthentication from "../hooks/useAuthentication";
+import { useNavigate } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: theme.spacing(8)
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: theme.spacing(8),
   },
   form: {
-    width: '100%',
-    marginTop: theme.spacing(1)
+    width: "100%",
+    marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 function LoginScreen() {
   const classes = useStyles();
+  const { isLoggedIn, login } = useAuthentication();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    }
+  }, [isLoggedIn]);
 
   return (
     <Container maxWidth="xs" className={classes.root}>
       <Typography variant="h5">Log in</Typography>
-      <form className={classes.form} noValidate>
+      <form className={classes.form} noValidate onSubmit={login}>
         <TextField
           variant="outlined"
           margin="normal"
