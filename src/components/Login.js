8edc96d45286f0a51c3d7fc421d1b9536c929354
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-01 11:53:17 +0000

    Added error message for login validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,74 +1,92 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography, TextField, Button } from "@material-ui/core";
-import useAuthentication from "../hooks/useAuthentication";
 import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginTop: theme.spacing(8),
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+root: {
+display: "flex",
+flexDirection: "column",
+alignItems: "center",
+marginTop: theme.spacing(8),
+},
+form: {
+width: "100%",
+marginTop: theme.spacing(1),
+},
+submit: {
+margin: theme.spacing(3, 0, 2),
+},
 }));
 
-function LoginScreen() {
-  const classes = useStyles();
-  const { isLoggedIn, login } = useAuthentication();
-  const navigate = useNavigate();
+function LoginScreen({login, isLoggedIn}) {
+const classes = useStyles();
+const navigate = useNavigate();
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate("/");
-    }
-  }, [isLoggedIn]);
+useEffect(() => {
+if (isLoggedIn) {
+navigate("/");
+}
+}, [isLoggedIn]);
 
-  return (
-    <Container maxWidth="xs" className={classes.root}>
-      <Typography variant="h5">Log in</Typography>
-      <form className={classes.form} noValidate onSubmit={login}>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-        >
-          Log in
-        </Button>
-      </form>
-    </Container>
-  );
+const handleSubmit = (e) => {
+e.preventDefault();
+if (!username || !password) {
+setErrorMessage("Please enter a username and password");
+return;
+}
+login({ username, password });
+};
+
+return (
+<Container maxWidth="xs" className={classes.root}>
+<Typography variant="h5">Log in</Typography>
+{errorMessage && <Typography color="error">{errorMessage}</Typography>}
+<form className={classes.form} noValidate onSubmit={handleSubmit}>
+<TextField
+variant="outlined"
+margin="normal"
+required
+fullWidth
+id="username"
+label="Username"
+name="username"
+value={username}
+onChange={(e) => setUsername(e.target.value)}
+autoComplete="username"
+autoFocus
+/>
+<TextField
+variant="outlined"
+margin="normal"
+required
+fullWidth
+name="password"
+label="Password"
+type="password"
+id="password"
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+autoComplete="current-password"
+/>
+<Button
+       type="submit"
+       fullWidth
+       variant="contained"
+       color="primary"
+       className={classes.submit}
+     >
+Log in
+</Button>
+</form>
+</Container>
+);
 }
 
 export default LoginScreen;
+
+
+
