96705d5f6bf5e1a61a6c2666b5d7c7893c4aeb3a
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-01 22:02:05 +0000

    Redirect to login screen on token expiry

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
 import { AuthContext } from "./AuthContext";
 
 function Home() {
@@ -8,23 +7,9 @@ function Home() {
   const [isLoggedIn, setIsLoggedIn, tokenValidity] = useContext(AuthContext);
 
   useEffect(() => {
-    const id = setInterval(() => {
-      // Decode the access token to get the payload
-      const accessToken = localStorage.getItem("AccessToken");
-      if (!!accessToken) {
-        const decodedToken = jwtDecode(accessToken);
-        console.log(decodedToken.exp * 1000 - Date.now());
-        // Check the exp claim to see if the token has expired
-        if (decodedToken.exp * 1000 < Date.now()) {
-          // The token has expired, so log out the user
-          localStorage.removeItem("AccessToken");
-          setIsLoggedIn(false);
-          navigate("/login");
-        }
-      }
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
+    if (!tokenValidity && !isLoggedIn) navigate("/login");
+  }, [tokenValidity]);
+
   return (
     <div className="home-container">
       <h1>Welcome to the Home page!</h1>
