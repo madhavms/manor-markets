b66ae25de9373a41f729c450825ffcefe33b84b1
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-01 22:01:15 +0000

    Redirect to login screen on token expiry

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
 
 function About() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn, tokenValidity] = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!tokenValidity) {
+      navigate("/login")
+    };
+  }, [tokenValidity]);
   return (
     <div className="about-container">
       <h1>About Us</h1>
