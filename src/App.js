271c47224e5c491dca1ea68fb5ab39744709d2ab
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-01 09:57:10 +0000

    Added protected routes which check authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import "./App.css";
 import ManorMarketsNavbar from "./components/NavBar";
-import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import {Products,Contact} from "./components/ProductsContact";
-
+import { Products, Contact } from "./components/ProductsContact";
+import LoginScreen from "./components/Login";
+import useAuthentication from "./hooks/useAuthentication";
 
 function App() {
+  const { isLoggedIn, login, logout } = useAuthentication();
   return (
     <BrowserRouter>
-      <ManorMarketsNavbar />
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/about" element={<About/>} />
-        <Route exact path="/products" element={<Products/>} />
-        <Route exact path="/contact" element={<Contact/>} />
-      </Routes>
+      <ManorMarketsNavbar {...{ isLoggedIn, logout }} />
+      {isLoggedIn ? (
+        <Routes>
+          <Route exact path="*" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="*" element={<LoginScreen login={login} />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
