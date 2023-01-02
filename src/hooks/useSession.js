27165e158a956c6992f2a8d2c499826456f08a87
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-02 11:55:49 +0000

    Created separate function for token expiry calculation

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -6,23 +6,27 @@ export const useSession = ({
   setIsLoggedIn,
 }) => {
 
+  const accessTokenExpiryTime = (accessToken) => {
+    const decodedToken = jwtDecode(accessToken);
+    const timeRemainingInSeconds = ((decodedToken.exp * 1000) - Date.now()) / 1000;
+    const minutes = Math.floor(timeRemainingInSeconds / 60);
+    const seconds = Math.floor(timeRemainingInSeconds % 60);
+    const secondString = seconds < 10 ? `0${seconds}` : `${seconds}`
+    const minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`
+    const timeOutput = `${minuteString}:${secondString}`;
+    return {timeOutput,timeRemainingInSeconds}
+  }
+
   const [timeRemaining, setTimeRemaining] = useState('00:00');
   useEffect(() => {
     const id = setInterval(() => {
       // Decode the access token to get the payload
       const accessToken = localStorage.getItem("AccessToken");
       if (!!accessToken) {
-        const decodedToken = jwtDecode(accessToken);
-        console.log("decode",decodedToken)
-        const timeRemainingInSeconds = ((decodedToken.exp * 1000) - Date.now()) / 1000;
-        const minutes = Math.floor(timeRemainingInSeconds / 60);
-        const seconds = Math.floor(timeRemainingInSeconds % 60);
-        const secondString = seconds < 10 ? `0${seconds}` : `${seconds}`
-        const minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`
-        const timeOutput = `${minuteString}:${secondString}`;
+        const {timeOutput,timeRemainingInSeconds} = accessTokenExpiryTime(accessToken);
         setTimeRemaining(timeOutput);
         // Check the exp claim to see if the token has expired
-        if (decodedToken.exp * 1000 < Date.now()) {
+        if (timeRemainingInSeconds < 0) {
           // The token has expired, so log out the user
           localStorage.removeItem("AccessToken");
           setTokenValidity(false);
