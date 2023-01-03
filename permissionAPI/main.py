ac60ed4f7046279261218ab43f78576d013f9809
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-03 10:43:33 +0000

    Prettier Formatter

--- permissionAPI/main.py
+++ permissionAPI/main.py
@@ -30,6 +30,7 @@ Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = "users"
 
@@ -37,38 +38,48 @@ class User(Base):
     username = Column(String, index=True, unique=True)
     password_hash = Column(String)
 
+
 Base.metadata.create_all(bind=engine)
+
 
 class UserCreate(BaseModel):
     username: str
     password: str
 
+
 class UserOut(BaseModel):
     id: int
     username: str
+
 
 class Credentials(BaseModel):
     username: str
     password: str
 
+
 @app.post("/users/", response_model=UserOut)
 def create_user(user: UserCreate):
     session = Session()
-    existing_user = session.query(User).filter(User.username == user.username).first()
+    existing_user = session.query(User).filter(
+        User.username == user.username).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already exists")
-    new_user = User(username=user.username, password_hash=generate_password_hash(user.password))
+    new_user = User(username=user.username,
+                    password_hash=generate_password_hash(user.password))
     session.add(new_user)
     session.commit()
     return UserOut(id=new_user.id, username=new_user.username)
+
 
 @app.post("/login")
 def login(credentials: Credentials):
     user = authenticate_user(credentials.username, credentials.password)
     if not user:
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
+        raise HTTPException(
+            status_code=401, detail="Incorrect username or password")
     print(f'The user id is {user.id}')
     return {"access_token": create_access_token(user.id)}
+
 
 def authenticate_user(username: str, password: str):
     session = Session()
@@ -79,8 +90,10 @@ def authenticate_user(username: str, password: str):
         return None
     return user
 
+
 def generate_password_hash(password: str):
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
 
 def create_access_token(user_id: int):
     payload = {
