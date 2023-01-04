113f22d62eaf1e6a969f3f17509b418b23181184
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-04 12:04:04 +0000

    Handle service down state

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -5,20 +5,23 @@ const useAuthentication = () => {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
 
   const login = async ({ username, password, setErrorMessage }) => {
-    const response = await fetch("http://localhost:8000/login", {
-      method: "POST",
-      body: JSON.stringify({ username, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await response.json();
-    if (response.ok) {
-      setIsLoggedIn(true);
-      localStorage.setItem("isLoggedIn", true);
-      localStorage.setItem("sessionValidity", true);
-      localStorage.setItem("AccessToken", JSON.stringify(data));
-    } else {
-      console.log("Error:", data.detail);
-      setErrorMessage(data.detail);
+    try {
+      const response = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setIsLoggedIn(true);
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("sessionValidity", true);
+        localStorage.setItem("AccessToken", JSON.stringify(data));
+      } else {
+        setErrorMessage(data.detail);
+      }
+    } catch (error) {
+      setErrorMessage("Something went wrong. Please try again later.");
     }
   };
 
