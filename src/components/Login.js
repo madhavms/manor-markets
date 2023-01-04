7342b0a5e863ee42d5d371b8eb008a814d048799
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-04 13:59:22 +0000

    Removed unused imports

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography, TextField, Button } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
