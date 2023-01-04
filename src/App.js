22adea91243ceb5645699bc3b6be15d2adbdaeb5
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-04 13:58:40 +0000

    Provided HOC wrapper for session validation

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ import About from "./components/About";
 import { Products, Contact } from "./components/ProductsContact";
 import LoginScreen from "./components/Login";
 import useAuthentication from "./hooks/useAuthentication";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "./components/AuthContext";
-
+import SessionValidator from "./components/SessionValidator";
 
 function App() {
-  const {login, logout, error} = useAuthentication();
-  const {isLoggedIn, tokenValidity, timeRemaining} = useContext(AuthContext);
+  const { login, logout, error } = useAuthentication();
+  const { isLoggedIn, tokenValidity, timeRemaining } = useContext(AuthContext);
 
   return (
     <BrowserRouter>
-      <ManorMarketsNavbar {...{ isLoggedIn, logout, tokenValidity, timeRemaining }} />
+      <ManorMarketsNavbar
+        {...{ isLoggedIn, logout, tokenValidity, timeRemaining }}
+      />
       {isLoggedIn && tokenValidity ? (
         <Routes>
-          <Route exact path="*" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/products" element={<Products />} />
-          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="*" element={<SessionValidator><Home /></SessionValidator>} />
+          <Route exact path="/about" element={<SessionValidator><About /></SessionValidator>} />
+          <Route exact path="/products" element={<SessionValidator><Products /></SessionValidator>} />
+          <Route exact path="/contact" element={<SessionValidator><Contact /></SessionValidator>} />
         </Routes>
       ) : (
         <Routes>
-          <Route path="*" element={<LoginScreen login={login} error={error} />} />
+          <Route
+            path="*"
+            element={<LoginScreen login={login} error={error} />}
+          />
         </Routes>
       )}
     </BrowserRouter>
