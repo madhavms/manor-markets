48f0b3b36969f735527b2d5ca8f53738d139f5da
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-05 01:02:34 +0000

    Moved the utils functions to separate module

---
+++ src/utils/SessionUtils.js
@@ -0,0 +1,33 @@
+import jwtDecode from "jwt-decode";
+
+export const refreshAccessToken = async () => {
+    try {
+      const refreshToken = localStorage.getItem('refreshToken');
+      console.log(refresh_token);
+      const response = await fetch("http://localhost:8000/refresh-token", {
+        method: "POST",
+        body: JSON.stringify({ "refresh_token": refreshToken }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      console.log(data);
+      const {access_token} = data;
+      console.log(access_token);
+      localStorage.setItem('accessToken', access_token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  export const accessTokenExpiryTime = (accessToken) => {
+    const decodedToken = jwtDecode(accessToken);
+    const timeRemainingInSeconds = ((decodedToken.exp * 1000) - Date.now()) / 1000;
+    let minutes = Math.floor(timeRemainingInSeconds / 60);
+    let seconds = Math.floor(timeRemainingInSeconds % 60);
+    minutes = minutes < 0 ? 0 : minutes;
+    seconds = seconds < 0 ? 0 : seconds;
+    const secondString = seconds < 10 ? `0${seconds}` : `${seconds}`
+    const minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`
+    const timeOutput = `${minuteString}:${secondString}`;
+    return {timeOutput,timeRemainingInSeconds}
+  }
